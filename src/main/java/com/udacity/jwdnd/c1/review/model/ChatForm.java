12bf5320c7925fd6409a29439351e2fe23c8d89a
scottseo000 <scottseo96@gmail.com>
2024-10-04 15:12:04 -0400

    added chatForm and chatMessage

--- src/main/java/com/udacity/jwdnd/c1/review/model/ChatForm.java
+++ src/main/java/com/udacity/jwdnd/c1/review/model/ChatForm.java
@@ -1,4 +1,32 @@
 package com.udacity.jwdnd.c1.review.model;
 
 public class ChatForm {
+    //POJO to store username, message type, message contents
+    private String username;
+    private String messageType;
+    private String messageText;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessageType() {
+        return messageType;
+    }
+
+    public void setMessageType(String messageType) {
+        this.messageType = messageType;
+    }
+
+    public String getMessageText() {
+        return messageText;
+    }
+
+    public void setMessageText(String messageText) {
+        this.messageText = messageText;
+    }
 }
--- src/main/java/com/udacity/jwdnd/c1/review/model/ChatMessage.java
+++ src/main/java/com/udacity/jwdnd/c1/review/model/ChatMessage.java
@@ -1,4 +1,23 @@
 package com.udacity.jwdnd.c1.review.model;
 
 public class ChatMessage {
+    //POJO to store chat message and user
+    private String username;
+    private String message;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
