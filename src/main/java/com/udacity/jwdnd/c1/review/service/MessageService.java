9f4d1fb8bdc9bfe13d08f88b2220bb14f71d1f58
scottseo000 <scottseo96@gmail.com>
2024-10-04 15:17:27 -0400

    updated MessageService

--- src/main/java/com/udacity/jwdnd/c1/review/service/MessageService.java
+++ src/main/java/com/udacity/jwdnd/c1/review/service/MessageService.java
@@ -1,26 +1,37 @@
 package com.udacity.jwdnd.c1.review.service;
 
+import com.udacity.jwdnd.c1.review.model.ChatForm;
+import com.udacity.jwdnd.c1.review.model.ChatMessage;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MessageService {
-    private String Message;
 
-    public MessageService(String message) {
-        this.Message = message;
-    }
-
-    public String uppercase() {
-        return this.Message.toUpperCase();
-    }
-
-    public String lowercase() {
-        return this.Message.toLowerCase();
-    }
+    private List<ChatMessage> chatMessages;
 
     @PostConstruct
     public void postConstruct() {
-        System.out.println("Creating MessageService bean");
+        this.chatMessages = new ArrayList<>();
+    }
+
+    public void addMessage(ChatForm chatForm) {
+        ChatMessage message = new ChatMessage();
+        message.setUsername(chatForm.getUsername());
+        if(chatForm.getMessageType() == "Shout") {
+            message.setMessage(chatForm.getMessageText().toUpperCase());
+        } else if (chatForm.getMessageType() == "Whisper") {
+            message.setMessage(chatForm.getMessageText().toLowerCase());
+        } else {
+            message.setMessage(chatForm.getMessageText());
+        }
+        this.chatMessages.add(message);
+    }
+
+    public List<ChatMessage> getChatMessages() {
+        return chatMessages;
     }
 }
