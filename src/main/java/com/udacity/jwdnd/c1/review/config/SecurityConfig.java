77bee1c34ec7c43d650b7847cae5bb5d6fc6f8a2
scottseo000 <scottseo96@gmail.com>
2024-10-08 16:30:34 -0400

    added logic to HashService and UserService

--- src/main/java/com/udacity/jwdnd/c1/review/config/SecurityConfig.java
+++ src/main/java/com/udacity/jwdnd/c1/review/config/SecurityConfig.java
@@ -1,4 +1,12 @@
 package com.udacity.jwdnd.c1.review.config;
 
-public class SecurityConfig {
+
+//TODO figure out why WebSecurityConfigurerAdapter isnt importing
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 }
--- src/main/java/com/udacity/jwdnd/c1/review/service/UserService.java
+++ src/main/java/com/udacity/jwdnd/c1/review/service/UserService.java
@@ -1,4 +1,36 @@
 package com.udacity.jwdnd.c1.review.service;
 
+import com.udacity.jwdnd.c1.review.mapper.UserMapper;
+import com.udacity.jwdnd.c1.review.model.User;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
 public class UserService {
+    private final UserMapper userMapper;
+    private final HashService hashService;
+
+    public UserService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    public boolean isUsernameAvailable(String username) {
+        return userMapper.getUser(username) == null;
+    }
+
+    public User getUser(String username) {
+        return userMapper.getUser(username);
+    }
+
+    public int createUser(User user) {
+        SecureRandom rand = new SecureRandom();
+        byte[] salt = new byte[16];
+        rand.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPass = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        return userMapper.insert(new User(null, user.getUsername(), encodedSalt, hashedPass, user.getFirstName(), user.getLastName()));
+    }
 }
