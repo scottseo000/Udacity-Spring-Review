bffe1354db0d39e5097bf93769ff011cf987a007
scottseo000 <scottseo96@gmail.com>
2024-10-08 14:17:13 -0400

    reenabled spring security

--- src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
+++ src/main/java/com/udacity/jwdnd/c1/review/ReviewApplication.java
@@ -12,22 +12,5 @@ public class ReviewApplication {
 		SpringApplication.run(ReviewApplication.class, args);
 	}
 
-	@Bean
-	public String message() {
-		System.out.println("Creating message bean");
-		return "hello, spring!";
-	}
-
-	@Bean
-	public String uppercaseMessage(MessageService messageService) {
-		System.out.println("Creating uppercaseMessage bean");
-		return messageService.uppercase();
-	}
-
-	@Bean
-	public String lowercaseMessage (MessageService messageService) {
-		System.out.println("Creating lowercaseMessage bean");
-		return messageService.lowercase();
-	}
 }
 
