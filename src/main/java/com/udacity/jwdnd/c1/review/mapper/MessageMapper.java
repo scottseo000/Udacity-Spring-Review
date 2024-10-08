421aa8215b5b8e58873f67d2cb60b0f575ff1be7
scottseo000 <scottseo96@gmail.com>
2024-10-08 14:30:58 -0400

    added User model and mappers for the two added tables

---
+++ src/main/java/com/udacity/jwdnd/c1/review/mapper/MessageMapper.java
@@ -0,0 +1,19 @@
+package com.udacity.jwdnd.c1.review.mapper;
+
+import com.udacity.jwdnd.c1.review.model.ChatMessage;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+public interface MessageMapper {
+    //Interacts with MESSAGE table
+
+    @Select("SELECT * FROM MESSAGES")
+    List<ChatMessage> getAllMessages();
+
+    @Insert("INSERT INTO MESSAGES (username, messagetext) VALUES(#{username}, #{messageText})")
+    @Options(useGeneratedKeys = true, keyProperty = "messageId")
+    int addMessage(ChatMessage chatMessage);
+}
